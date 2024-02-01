5059f9c7b4710ca01a738f1fd435c58e343e5883
한진아 <wlsdk6395@naver.com>
2024-02-01 21:28:23 +0900

    변수 연습

---
+++ 02.java/step00_prac/src/step00_prac/Ex01Variable.java
@@ -0,0 +1,21 @@
+package step00_prac;
+
+public class Ex01Variable {
+
+	public static void main(String[] args) {
+
+		int year = 0;
+		int age = 24;
+		System.out.println(year);
+		System.out.println(age);
+		
+		System.out.println(age + 2000);
+		System.out.println(age + 1);
+		
+		// 문자열 작성 시 "" 필요 / 숫자는 그냥 작성 가능
+		System.out.println("Java");
+		System.out.println("Java" + 8.0);
+		System.out.println(7);
+	}
+
+}

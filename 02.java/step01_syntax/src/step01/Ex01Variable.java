24b329d19bb1dd97d9efeed32c2f3006b56b6ba0
한진아 <wlsdk6395@naver.com>
2024-02-01 21:29:54 +0900

    기본타입, 참조타입

---
+++ 02.java/step01_syntax/src/step01/Ex01Variable.java
@@ -0,0 +1,40 @@
+package step01;
+
+public class Ex01Variable {
+
+	public static void main(String[] args) {
+		// 기본타입
+
+		// 정수
+		byte vb1 = -128;
+		// byte vb2 = 128;
+		short vb2 = 128;
+		
+		int vi1 = 10;
+		int vi2 = 0B1010;
+		
+		long vl1 = 10;
+		long vl2 = 10000000000L;
+		
+		// 실수
+		float vf1 = 3.14F;
+		double vd1 = 3.14;
+		
+		// 문자 ''
+		char vc1 = 'A'; 
+		char vc2 = 65;
+		
+		// 논리
+		boolean isTrue = true;
+		boolean isFalse = false;
+		
+		System.out.println(vc2);
+		
+		// 참조타입
+		// 문자열 ""
+		String str = "hello";
+		System.out.println(str);
+		
+	}
+
+}
